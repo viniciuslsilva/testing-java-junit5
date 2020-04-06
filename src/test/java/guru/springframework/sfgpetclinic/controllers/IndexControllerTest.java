e9a1025cd442294441cc759bd5b896f361f1bcbb
Vinicius Silva <vinicius.luiz19@gmail.com>
2020-04-06 00:47:16 -0300

    Adding dependent assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -0,0 +1,31 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+    IndexController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new IndexController();
+    }
+
+    @Test
+    void index() {
+        assertEquals("index", controller.index());
+        assertEquals("index", controller.index(), "Wrong View Returned");
+
+        assertEquals("index", controller.index(), () -> "Another Expansive Message " +
+                "Make me only if you have to");
+    }
+
+    @Test
+    void oupsHandler() {
+        assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "This some expensive Message to build " +
+                "for my test");
+    }
+}
