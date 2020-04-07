078e351eed6d06d3fe9c45a20ef7eba59926a6ab
Vinicius Silva <vinicius.luiz19@gmail.com>
2020-04-07 20:52:01 -0300

    Completed assignment

---
+++ src/test/java/guru/springframework/fauxspring/ModelMapImpl.java
@@ -0,0 +1,25 @@
+package guru.springframework.fauxspring;
+
+import guru.springframework.sfgpetclinic.fauxspring.Model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ModelMapImpl  implements Model {
+    Map<String, Object> map = new HashMap<>();
+
+    @Override
+    public void addAttribute(String key, Object o) {
+
+        map.put(key, o);
+    }
+
+    @Override
+    public void addAttribute(Object o) {
+        // do nothing...
+    }
+
+    public Map<String, Object> getMap() {
+        return map;
+    }
+}
