ec7a85c76ad275acefabecf0343a214639cf2143
Vinicius Silva <vinicius.luiz19@gmail.com>
2020-04-07 22:03:25 -0300

    Adding di example

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,9 +1,7 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.ModelTests;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -34,5 +32,11 @@ class PersonTest implements ModelTests {
     @DisplayName("My Repeated Test")
     void myRepeatedTest() {
         //todo - impl
+    }
+
+    @RepeatedTest(5)
+    void myRepeatedTestWithDI(TestInfo testInfo, RepetitionInfo repetitionInfo) {
+        System.out.println(testInfo.getDisplayName() + ": " + repetitionInfo.getCurrentRepetition());
+
     }
 }
