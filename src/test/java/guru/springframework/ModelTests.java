26b9baaa87ad08f5d66cf92cc3738c78594547c2
Vinicius Silva <vinicius.luiz19@gmail.com>
2020-04-07 22:08:43 -0300

    Completing assignment

--- src/test/java/guru/springframework/ModelTests.java
+++ src/test/java/guru/springframework/ModelTests.java
@@ -1,7 +1,13 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInfo;
 
 @Tag("model")
 public interface ModelTests {
+    @BeforeEach
+    default void beforeEachConsoleOutputer(TestInfo testInfo){
+        System.out.println("Running Test - " + testInfo.getDisplayName());
+    }
 }
---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonRepeatedTests.java
@@ -0,0 +1,28 @@
+package guru.springframework.sfgpetclinic.model;
+
+import guru.springframework.ModelRepeatedTests;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.TestInfo;
+
+public class PersonRepeatedTests implements ModelRepeatedTests {
+
+    @RepeatedTest(value = 10, name = "{displayName} : {currentRepetition} - {totalRepetitions}")
+    @DisplayName("My Repeated Test")
+    void myRepeatedTest() {
+        //todo - impl
+    }
+
+    @RepeatedTest(5)
+    void myRepeatedTestWithDI(TestInfo testInfo, RepetitionInfo repetitionInfo) {
+        System.out.println(testInfo.getDisplayName() + ": " + repetitionInfo.getCurrentRepetition());
+
+    }
+
+    @RepeatedTest(value = 5, name = "{displayName} : {currentRepetition} | {totalRepetitions}")
+    @DisplayName("My Assignment Repeated Test")
+    void myAssignmentRepeated() {
+        //todo impl
+    }
+}
